bbee6fe49a65978098eb99bf9407814d16e3f800
Bo Chen <bchen2@woolworths.com.au>
2023-08-01 16:30:18 +1000

    feat: firebase setup

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,9 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-};
+    apiKey: "AIzaSyDKuD-VGgHH1XGMxRi5amufeJKBNOSsGHA",
+    authDomain: "genai-bc6ba.firebaseapp.com",
+    projectId: "genai-bc6ba",
+    storageBucket: "genai-bc6ba.appspot.com",
+    messagingSenderId: "504064881467",
+    appId: "1:504064881467:web:a58ff7d37670068eaebd1e"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
